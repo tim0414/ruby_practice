e323a27cdd4c16d9c1178fbcba81ba0a71742abf
tim <tim_chen@timteki-MacBook-Air.local>
2018-07-19 10:58:10 +0800

    modify hash example

--- user_define_hash.rb
+++ user_define_hash.rb
@@ -17,7 +17,14 @@ class B
         @b = b
     end
     def eql?(o)
-        self.b == o.b+1
+        if self.b%2==0 then self.b+1 == o.b
+        else self.b == o.b-1
+        end
+    end
+    def hash
+        if self.b%2==0 then self.b.hash+(self.b+1).hash
+        else self.b.hash+(self.b-1).hash
+        end
     end
 end
 
@@ -26,20 +33,38 @@ class C
     def initialize(c)
         @c = c
     end
+    def eql?(o)
+        self.c == o.c
+    end
+    def hash
+        10
+    end
 end
 
 
 a1 = A.new(10)
 a2 = A.new(10)
 
-b1 = B.new(10)
-b2 = B.new(11)
+b1 = B.new(11)
+b2 = B.new(10)
 
 c1 = C.new(10)
+c2 = C.new(10)
+c3 = C.new(12)
 
-h = {a1 => "a1 hash value", b1 => "b1 hash value"}
+h = {a1 => "a1 hash value", b1 => "b1 hash value", c1 => "c1 hash value", b2 => "b2 hash key"}
 print "hash code: #{c1.hash}, object id: #{c1.object_id}\n "
 print b1.b+1, "\n"
 
-print h[a2], " ", h[b2]
-        
+print h, "\n"
+
+puts "b1 is eql? to b2: #{b1.eql?b2} \n"
+puts b1.hash == b2.hash
+puts h[b2] # how does hash compare their key? b2.eql?b1
+puts h[b1]
+
+
+puts [1,2,3] === [1,2,3]
+#print "\n #{b1.hash} #{b2.hash}"
+print "c1 is eql to c2?: #{c1.eql?c2} "
+print h[c2]
