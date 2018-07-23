80528f4e7781b39ea198b567fda08aa21f6ce12a
tim0414 <tim04142013@gmail.com>
2018-07-24 06:30:02 +0800

    modify compare and flip flop

--- compare.rb
+++ compare.rb
@@ -1,7 +1,13 @@
-1 == 1.0    # => true
-1.eql?(1.0) # => false
-puts (1.0..7.0)===3 # => true
-puts (1.0..7.0)===8 # => false
+
+1 == 1.0        # => true
+1.eql?(1.0)     # => false
+1 === 1.0       # => true
+(1.0..7.0)===3  # => true
+(1.0..7.0)===8  # => false
+1.equal?1       # => true
+(5*(10**18)).equal?(5*(10**18)) # => false
+
+puts 2 <=> 3
 
 
 puts "abc" == "abc"  # => true
@@ -9,3 +15,35 @@ puts "abc".eql?"abc" # => true
 puts /\d+/ === "123" # => true
 
 puts 2**3**2
+
+puts "a" <=> "a"  # => -1
+puts "a" <=> "a"  # => 0
+puts "b" <=> "a"  # => 1
+puts "a" <=> "ab" # => -1
+
+class Point
+    attr_accessor :x, :y
+    include Comparable
+    def initialize(x,y)
+        @x, @y = x, y
+    end
+    def <=>(o)
+        return 0 if (@x==o.x and @y==o.y)
+        return 1 if (@x>=o.x and @y>=o.y)
+        return -1 if (@x <= o.x and @y<= o.y)
+        
+    end
+end
+
+p1 = Point.new(1,1)
+p2 = Point.new(2,1)
+p3 = Point.new(3,0)
+
+puts p1>p2
+puts p1==p2
+puts p1<p2
+
+p1<=>p3 # => nil
+p1>p3
+p1==p3
+p1<p3
--- flip_flop.rb
+++ flip_flop.rb
@@ -8,3 +8,36 @@ end
 (1..10).each {|x| print x if x==3..x>=3} # => 3
 
 (1..10).each {|x| print x if x==3...x<=3} # => 34
+
+14 % 5 % 3 # => 1
+
+2**3**2 # => 2**9 = 512
+
+puts
+str = "\n"
+
+arr = [1,2,3,3]
+arr << 4 # => [1,2,3,4]
+arr.push(5) # => [1,2,3,3,4,5]
+arr << [6,7,8] # => [1,2,3,3,4,5,[6,7,8]]
+
+arr.pop # => [6,7,8]
+arr.shift # => 1
+arr.delete(3) # => 3
+puts arr.delete_at(2) # => 5
+
+arr # => [2,4]
+
+
+print arr[-2,10]
+
+#puts arr.shift
+
+nils = Array.new(3) # => [nul, nil, nil]
+copy = Array.new(nils)
+
+#puts nil.object_id
+#puts copy.object_id
+
+print 1===1.0
+print FIXNUM_MAX
